c5bcbf33be456a14e34af6e044225572bec5bb7a
MDArshad555 <thisisarshad555@gmail.com>
2024-08-31 12:13:55 +0530

    db connection

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "./constants.js";
+import connectDB from "./db/index.js";
+
+/*
+import express from "express"
+const app=express()
+ 
+;  (async()=>{
+    try{
+       await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+    app.on("error",()=>{
+       conslotchange.log("error",error) 
+       throw error
+    })
+    app.listen(process.env.PORT,()=>{
+        console.log(`app is istening on port ${process.env.PORT}`)
+    })
+    }catch(error){
+       console.error("Error",error) 
+       throw err
+    }
+})()
+    */
