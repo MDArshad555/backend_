1c2c212bbaa2f32d0425ce5ce3cccf5552ce8a22
MDArshad555 <thisisarshad555@gmail.com>
2024-09-03 16:53:56 +0530

    jwt token added and bcrypt function

--- src/db/index.js
+++ src/db/index.js
@@ -12,3 +12,11 @@ import 'dotenv/config'
     }
  }
  export default connectDB()
+ .then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`server is running at port :${process.env.PORT}`)
+    })
+ })
+ .catch((err)=>{
+    console.log("mongo db connection failed",err); 
+ })
